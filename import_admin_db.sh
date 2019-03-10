d490462707eefb97b312578a655eeb24f75b8841
long <396667187@qq.com>
2019-03-10 15:52:16 +0800

    修改mysql导入脚本

--- import_admin_db.sh
+++ import_admin_db.sh
@@ -3,6 +3,6 @@
 # 导入 .env  环境变量
 source ./.env
 
-mysql -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < database/admin.sql
+mysql -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < database/admin.sql
 
 2>/dev/null
